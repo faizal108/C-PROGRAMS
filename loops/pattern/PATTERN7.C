c6999df211227351865976249b39cd043711d252
faizal108 <69467378+faizal108@users.noreply.github.com>
2020-09-17 04:38:26 -0700

    Update PATTERN7.C

--- loops/pattern/PATTERN7.C
+++ loops/pattern/PATTERN7.C
@@ -1,3 +1,11 @@
+/*Excepting output right angle triangle
+EX:user enter:4
+our output:
+1
+2 3
+4 5 6
+7 8 9 10
+==============================================================================*/
 #include<stdio.h>
 #include<conio.h>
 
